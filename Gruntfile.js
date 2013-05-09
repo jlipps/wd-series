bb7e4c9827d9720a579aa5685da829e21f54178f
Jonathan Lipps <jlipps@gmail.com>
2013-05-09 14:36:33 -0700

    add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: ['*.js', 'test/unit/*.js', 'test/functional/*.js']
+      , options: {
+        laxcomma: true
+        , es5: true
+        , trailing: true
+        , node: true
+        , strict: true
+      }
+    }
+    , mochaTest: {
+      unit: ['test/unit/*.js']
+      , functional: ['test/functional/*.js']
+    }
+    , mochaTestConfig: {
+      options: {
+        timeout: 60000,
+        reporter: 'spec'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'unit']);
+  grunt.registerTask('unit', 'mochaTest:unit');
+  grunt.registerTask('default', ['test']);
+};
+
